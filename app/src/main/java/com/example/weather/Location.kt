4268ad8c3d9ca0a6e3cb907f49335387718a384a
mxnul <manularatnayake007@gmail.com>
2023-11-30 21:22:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weather/Location.kt
@@ -0,0 +1,91 @@
+package com.example.weather
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class Location : AppCompatActivity() {
+    private lateinit var enterText: EditText
+    private lateinit var ltemp: TextView
+    private lateinit var lstatus: TextView
+    private lateinit var ldate: TextView
+    private lateinit var ltime: TextView
+    private lateinit var lcity: TextView
+    private lateinit var lvalueWind: TextView
+    private lateinit var lvalueHumidity: TextView
+    private lateinit var lvaluemax:TextView
+
+    private val apiKey = "f45574ec1948a319c775ac26ded0cc8d"
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+
+        enterText = findViewById(R.id.enterText)
+        ltemp = findViewById(R.id.ltemp)
+        lstatus = findViewById(R.id.lstatus)
+        ldate = findViewById(R.id.ldate)
+        ltime = findViewById(R.id.ltime)
+        lcity = findViewById(R.id.lcity)
+        lvalueWind = findViewById(R.id.lvalueWind)
+        lvalueHumidity = findViewById(R.id.lvalueHumidity)
+        lvaluemax = findViewById(R.id.lvaluemax)
+
+        enterText.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE || (event?.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Perform weather search for the entered city
+                loadDetails(enterText.text.toString())
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+    }
+
+   /////////////isuru///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private fun parseWeatherDetails(response: JSONObject) {
+        try {
+            val cityName = response.getString("name")
+            lcity.text = cityName
+
+            val main = response.getJSONObject("main")
+            val temp = main.getString("temp")
+            ltemp.text = "$temp °C"
+
+            val windl = response.getJSONObject("wind").getString("speed")
+            lvalueWind.text = "$windl"+"km/h"
+
+            val humidityl = response.getJSONObject("main").getString("humidity")
+            lvalueHumidity.text = "$humidityl"
+
+
+            val maxl = response.getJSONObject("main").getString("temp_max")
+            lvaluemax.text = "$maxl"
+
+
+            val date = SimpleDateFormat("EEEE, MMMM d", Locale.getDefault())
+            val time = SimpleDateFormat("h:mm a", Locale.getDefault())
+            val currentDate = date.format(Date())
+            val currentTime = time.format(Date())
+
+            ldate.text = currentDate
+            ltime.text = currentTime
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.weather
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import android.location.Address
+import android.location.Geocoder
+import android.widget.Button
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var progressDialog: ProgressDialog? = null
+    private lateinit var temp: TextView
+    private lateinit var status: TextView
+    private lateinit var date: TextView
+    private lateinit var time: TextView
+    private lateinit var valueWind: TextView
+    private lateinit var valueHumidity: TextView
+    private lateinit var valuemax:TextView
+    private lateinit var btnLoc:Button
+
+
+    private val apiKey = "f45574ec1948a319c775ac26ded0cc8d"
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        temp = findViewById(R.id.temp)
+        status = findViewById(R.id.status)
+        date = findViewById(R.id.date)
+        time = findViewById(R.id.time)
+        valueWind = findViewById(R.id.valueWind)
+        valueHumidity = findViewById(R.id.valueHumidity)
+        valuemax = findViewById(R.id.valuemax)
+        btnLoc= findViewById(R.id.btnLoc)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        requestLocationPermission()
+
+
+        getCurrentLocationAndWeather()
+
+        btnLoc.setOnClickListener {
+            // Navigate to Locations.Kt page
+            val intent = Intent(this,Location::class.java)
+            startActivity(intent)
+        }
+    }
+
+   //senadi's part////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private fun loadWeather(latitude: Double, longitude: Double) {
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$apiKey"
+        showProgressDialog("Loading")
+
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { data ->
+                try {
+                    val description =
+                        data.getJSONArray("weather").getJSONObject(0).getString("description")
+                    status.text = description
+
+                    val temperature = data.getJSONObject("main").getString("temp")
+
+                    temp.text = "$temperature"+"°C"
+
+                    val wind = data.getJSONObject("wind").getString("speed")
+                    valueWind.text = "$wind"+"km/h"
+
+                    val humidity = data.getJSONObject("main").getString("humidity")
+                    valueHumidity.text = "$humidity"
+
+
+                    val max = data.getJSONObject("main").getString("temp_max")
+                    valuemax.text = "$max"
+
+                    val dateV = SimpleDateFormat("EEEE, MMMM d", Locale.getDefault())
+                    val timeV = SimpleDateFormat("h:mm a", Locale.getDefault())
+
+                    val currentDate = dateV.format(Date())
+                    val currentTime = timeV.format(Date())
+
+                    date.text = currentDate
+                    time.text = currentTime
+
+
+                    dismissProgressDialog()
+                } catch (e: Exception) {
+                    Log.e("Error", e.toString())
+                    dismissProgressDialog()
+                }
+            },
+            { error ->
+                Log.e("Response", error.toString())
+                // Handle error here
+                dismissProgressDialog()
+                showErrorToast("Error fetching weather data")
+            })
+
+        Volley.newRequestQueue(this).add(request)
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+    }
+
+    private fun showProgressDialog(message: String) {
+        progressDialog = ProgressDialog(this)
+        progressDialog?.setMessage(message)
+        progressDialog?.setCancelable(false)
+        progressDialog?.show()
+    }
+
+    private fun dismissProgressDialog() {
+        progressDialog?.dismiss()
+    }
+
+    private fun showErrorToast(message: String) {
+        Toast.makeText(this, "Error: $message", Toast.LENGTH_SHORT).show()
+    }
+}
